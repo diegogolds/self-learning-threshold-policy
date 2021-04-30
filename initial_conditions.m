a63ea9a2e217ec0660b8a44f57f02593c8c78dc7
diegogolds <83447505+diegogolds@users.noreply.github.com>
2021-04-30 11:58:07 +0200

    Create initial_conditions.m

---
+++ initial_conditions.m
@@ -0,0 +1,44 @@
+%This function generates vectors 'queues', 'tasks', 'green' and 'yellow'
+%corresponding to the initial occupancy state 'state' of a system with N
+%server pools, server pool size B and threshold l.
+function [queues, tasks, green, yellow] = initial_conditions(N, B, l, state)
+    %Queues:
+    queues = zeros(1, N);
+    undef = 1;
+    for j = undef : undef + N - state(1) - 1
+        queues(j) = 0;
+    end
+    undef = undef + N - state(1);
+    for i = 1 : B - 1
+        for j = undef : undef + state(i) - state(i + 1) - 1
+            queues(j) = i;
+        end
+        undef = undef + state(i) - state(i + 1);
+    end
+    for j = undef : undef + state(B) - 1
+        queues(j) = B;
+    end
+    %Tasks:
+    tasks = zeros(1, B * N);
+    undef = 1;
+    for i = 1 : N
+        for j = undef : undef + queues(i) - 1
+            tasks(j) = i;
+        end
+        undef = undef + queues(i);
+    end
+    %Green:
+    green = zeros(1, N);
+    for i = 1 : N
+        if queues(i) < l
+            green(i) = i;
+        end
+    end
+    %Yellow:
+    yellow = zeros(1, N);
+    for i = 1 : N
+        if queues(i) <= l
+            yellow(i) = i;
+        end
+    end
+end
