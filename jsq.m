1a37c31bb5aab33624c973129aa8f47e817af997
diegogolds <83447505+diegogolds@users.noreply.github.com>
2021-04-30 13:32:12 +0200

    Create jsq.m

---
+++ jsq.m
@@ -0,0 +1,81 @@
+%System parameters:
+lambda = 10.5;
+tf = 5; %simulation time.
+B = 41; %maximum number of tasks per server pool (much larger than lambda).
+N = 500; %number of server pools.
+%State variables:
+i = 1;
+t = 0;
+discarded = 0; %number of discarded tasks (should remain equal to zero).
+arrivals = 0; %number of arrivals.
+%Initial condition:
+state = zeros(1, B); %occupancy state of the system.
+for k = 1 : floor(lambda)
+    state(k) = N;
+end
+state(k + 1) = round((lambda - floor(lambda)) * N);
+[queues, tasks, ~, ~] = initial_conditions(N, B, floor(lambda), state);
+%Output data:
+events = round(2 * lambda * N * tf);
+T = zeros(events);
+D = zeros(events);
+Q = zeros(events, N);
+S = zeros(events, B);
+Q(1, :) = queues;
+S(1, :) = state;
+%Time until next event:
+arrival = exprnd(1 / (lambda * N));
+departure = exprnd(1 / sum(queues));
+[dt, event] = min([arrival departure]);
+%Main loop:
+while t + dt < tf
+    %Time update:
+    i = i + 1;
+    t = t + dt;
+    %Arrival:
+    if event == 1
+        arrivals = arrivals + 1;
+        [m, k] = min(queues);
+        if m < B
+            tasks = add(tasks, k(1));
+            queues(k(1)) = queues(k(1)) + 1;
+            state(queues(k(1))) = state(queues(k(1))) + 1;
+        else
+            discarded = discarded + 1;
+        end
+    end
+    %Departure:
+    if event == 2
+        k = draw(tasks);
+        tasks = remove(tasks, k);
+        state(queues(k)) = state(queues(k)) - 1;
+        queues(k) = queues(k) - 1;
+    end
+    %Output data update:
+    T(i) = t;
+    D(i) = discarded;
+    Q(i, :) = queues;
+    S(i, :) = state;
+    %Time until next event:
+    arrival = exprnd(1 / (lambda * N));
+    departure = exprnd(1 / sum(queues));
+    [dt, event] = min([arrival departure]);
+    %Waitbar:
+    wbar = waitbar(t / tf);
+end
+close(wbar);
+%Outoput data:
+T = T(1 : i);
+D = D(1 : i);
+Q = Q(1 : i, :);
+S = S(1 : i, :);
+save('jsq');
+%Check arrivals:
+disp(arrivals / (N * t));
+
+%System state across time:
+figure('Name', 'Normalized system state across time', 'NumberTitle', 'off');
+plot(T, S(:, floor(lambda)) / N, T, S(:, floor(lambda) + 1) / N, T, S(:, floor(lambda) + 2) / N);
+title('System state');
+xlabel('t');
+legend('q_{[\lambda]}', 'q_{[\lambda] + 1}', 'q_{[\lambda] + 2}');
